c654a5026a61b0daeb4347150f44ff699adea404
Aymerick <aymerick@jehanne.org>
2014-07-18 14:22:34 +0200

    adds google analytics page tracking

--- app/router.js
+++ app/router.js
@@ -5,7 +5,19 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-	this.route('about');
+  this.route('about');
+});
+
+// Cf. http://emberjs.com/guides/cookbook/helpers_and_components/adding_google_analytics_tracking/
+Router.reopen({
+  notifyGoogleAnalyticsPageView: function() {
+    if (window.ga) {
+      return window.ga('send', 'pageview', {
+        'page': this.get('url'),
+        'title': this.get('url')
+      });
+    }
+  }.on('didTransition'),
 });
 
 export default Router;
