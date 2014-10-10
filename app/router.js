9ee00aa32431ad66cdc3e839970e99465d029af6
Aymerick <aymerick@jehanne.org>
2014-10-10 18:23:19 +0200

    Adds /weekly page

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('about');
+  this.route('weekly');
 });
 
 // Cf. http://emberjs.com/guides/cookbook/helpers_and_components/adding_google_analytics_tracking/
